b1451f024b0bad0adb2715a789603754fafd16f2
yasser-eddouche <eddouchey@gmail.com>
2025-10-20 17:00:10 +0100

    3rd commit

---
+++ inventory-service/src/main/java/org/example/inventoryservice/config/RestRepositoryConfig.java
@@ -0,0 +1,14 @@
+package org.example.inventoryservice.config;
+
+import org.example.inventoryservice.entities.Product;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+
+@Configuration
+public class RestRepositoryConfig implements RepositoryRestConfigurer {
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistration cors){
+        config.exposeIdsFor(Product.class);
+    }
+}
