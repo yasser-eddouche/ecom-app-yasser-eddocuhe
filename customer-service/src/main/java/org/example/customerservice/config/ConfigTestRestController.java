157694aab57a236892519c60769a161a685d6a19
yasser-eddouche <eddouchey@gmail.com>
2025-11-16 19:45:17 +0100

    last part

--- customer-service/src/main/java/org/example/customerservice/config/ConfigTestRestController.java
+++ customer-service/src/main/java/org/example/customerservice/config/ConfigTestRestController.java
@@ -1,18 +1,30 @@
 package org.example.customerservice.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RefreshScope
 public class ConfigTestRestController {
-
+    @Value("${global.params.p1}")
+    private String p1;
+    @Value("${global.params.p2}")
+    private String p2;
     @Autowired
     private CustomerConfigParams customerConfigParams;
 
     @GetMapping("/config-test")
+    public Map<String,String> config(){
+        return Map.of("p1",p1,"p2",p2);
+    }
+
+    @GetMapping("/config-test2")
     public CustomerConfigParams getConfigParams() {
         return customerConfigParams;
     }
--- customer-service/src/main/java/org/example/customerservice/config/CustomerConfigParams.java
+++ customer-service/src/main/java/org/example/customerservice/config/CustomerConfigParams.java
@@ -2,6 +2,6 @@ package org.example.customerservice.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "customer.config")
+@ConfigurationProperties(prefix = "customer.params")
 public record CustomerConfigParams(int x, int y) {
 }
